243445f68c38d2f45e8da092c97e1c5e3f0113b5
Brian Lai <brianlai30@outlook.com>
2021-02-27 15:46:45 -0800

    Finish section 6, adding navigation, event emitters, components on clicks

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Curry Rice', 'Delicious curry over rice.', 'https://upload.wikimedia.org/wikipedia/commons/1/1d/Curry_rice.jpg'),
     new Recipe('Chirashi Bowl', 'Mixed sushi ingredients over rice.', 'https://upload.wikimedia.org/wikipedia/commons/b/ba/Chirashi_Tanuki_Sierra_Vista.jpg')
@@ -17,4 +18,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe): void {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
